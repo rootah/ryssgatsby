485d899abec65b9b73f820690dbf3c64ce6cf0c8
makar theryss <digitalryss@gmail.com>
2020-07-31 02:15:45 +0600

    error founded!

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `http://theryss.gq, // Site domain. Do not include a trailing slash!
+    siteUrl: `http://theryss.gq`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
